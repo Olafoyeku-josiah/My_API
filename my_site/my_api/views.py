e58b41ab936acf98ed69ca72e792e48ed1c0dba5
olafoyeku-josiah <olafoyekujosiah@gmail.com>
2025-08-12 13:30:42 -0700

    new changes

--- my_site/my_api/views.py
+++ my_site/my_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Blogpost
+from serializers import BlogpostSerializer
 # Create your views here.
+class BlogpostListCreate(generics.ListCreateAPIView):
+    queryset=Blogpost.objects.all()
+    serializer_class= BlogpostSerializer
+
